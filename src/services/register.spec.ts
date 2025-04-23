f08904f19efc4a7f26bef72c100fa206820ec4b3
Nicholas Costa <nicholascostadev@gmail.com>
2025-04-23 18:54:03 -0300

    test: add first tests for register service

--- src/services/register.spec.ts
+++ src/services/register.spec.ts
@@ -1,5 +1,61 @@
-import { expect, test } from "vitest";
+import { InMemoryUsersRepository } from "@/repositories/in-memory/in-memory-users-repository";
+import { compare } from "bcryptjs";
+import { describe, expect, it } from "vitest";
+import { UserAlreadyExistsError } from "./errors/user-already-exists-error";
+import { RegisterService } from "./register";
 
-test("Check if it workds", () => {
-	expect(2 + 2).toBe(4);
+describe("Register Service", () => {
+	it("should hash user password upon registration", async () => {
+		const inMemoryUsersRepository = new InMemoryUsersRepository();
+		const registerService = new RegisterService(inMemoryUsersRepository);
+
+		const { user } = await registerService.execute({
+			name: "John Doe",
+			email: "john.doe@example.com",
+			password: "123456",
+		});
+
+		const isPasswordCorrectlyHashed = await compare(
+			"123456",
+			user.password_hash,
+		);
+
+		expect(isPasswordCorrectlyHashed).toBe(true);
+	});
+
+	it("should not be able to register with same email twice", async () => {
+		const inMemoryUsersRepository = new InMemoryUsersRepository();
+		const registerService = new RegisterService(inMemoryUsersRepository);
+
+		const email = "john.doe@example.com";
+
+		await registerService.execute({
+			name: "John Doe",
+			email,
+			password: "123456",
+		});
+
+		await expect(
+			registerService.execute({
+				name: "John Doe",
+				email,
+				password: "123456",
+			}),
+		).rejects.toBeInstanceOf(UserAlreadyExistsError);
+	});
+
+	it("should be able to register", async () => {
+		const inMemoryUsersRepository = new InMemoryUsersRepository();
+		const registerService = new RegisterService(inMemoryUsersRepository);
+
+		const email = "john.doe@example.com";
+
+		const { user } = await registerService.execute({
+			name: "John Doe",
+			email,
+			password: "123456",
+		});
+
+		expect(user.id).toEqual(expect.any(String));
+	});
 });
--- src/services/register.ts
+++ src/services/register.ts
@@ -1,4 +1,5 @@
 import type { UsersRepository } from "@/repositories/users-repository";
+import type { User } from "@prisma/client";
 import { hash } from "bcryptjs";
 import { UserAlreadyExistsError } from "./errors/user-already-exists-error";
 
@@ -8,10 +9,18 @@ type RegisterServiceRequest = {
 	password: string;
 };
 
+interface RegisterServiceResponse {
+	user: User;
+}
+
 export class RegisterService {
 	constructor(private usersRepository: UsersRepository) {}
 
-	async execute({ name, email, password }: RegisterServiceRequest) {
+	async execute({
+		name,
+		email,
+		password,
+	}: RegisterServiceRequest): Promise<RegisterServiceResponse> {
 		const password_hash = await hash(password, 6);
 
 		const userWithSameEmail = await this.usersRepository.findByEmail(email);
@@ -20,10 +29,12 @@ export class RegisterService {
 			throw new UserAlreadyExistsError();
 		}
 
-		await this.usersRepository.create({
+		const user = await this.usersRepository.create({
 			name,
 			email,
 			password_hash,
 		});
+
+		return { user };
 	}
 }
