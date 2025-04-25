5933a5658e9bc4bf4c4b7e1d18caba15fe66023f
Nicholas Costa <nicholascostadev@gmail.com>
2025-04-25 17:40:58 -0300

    feat: add initial checkin validations

--- src/services/check-in.spec.ts
+++ src/services/check-in.spec.ts
@@ -1,18 +1,56 @@
 import { InMemoryCheckInsRepository } from "@/repositories/in-memory/in-memory-check-ins-repository";
-import { hash } from "bcryptjs";
-import { beforeEach, describe, expect, it } from "vitest";
+import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
 import { CheckInService } from "./check-in";
 
-describe("Authenticate Service", () => {
+describe("Check In Service", () => {
 	let inMemoryCheckInsRepository: InMemoryCheckInsRepository;
 	let sut: CheckInService;
 
 	beforeEach(() => {
 		inMemoryCheckInsRepository = new InMemoryCheckInsRepository();
 		sut = new CheckInService(inMemoryCheckInsRepository);
+
+		vi.useFakeTimers();
+	});
+
+	afterEach(() => {
+		vi.useRealTimers();
 	});
 
 	it("should be able to check in", async () => {
+		const { checkIn } = await sut.execute({
+			gymId: "gym-01",
+			userId: "user-01",
+		});
+
+		expect(checkIn.id).toEqual(expect.any(String));
+	});
+
+	it("should not be able to check in twice in the same date", async () => {
+		vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0));
+		const { checkIn } = await sut.execute({
+			gymId: "gym-01",
+			userId: "user-01",
+		});
+
+		await expect(
+			sut.execute({
+				gymId: "gym-01",
+				userId: "user-01",
+			}),
+		).rejects.toBeInstanceOf(Error);
+		expect(checkIn.id).toEqual(expect.any(String));
+	});
+
+	it("should not be able to check in twice but in different dates", async () => {
+		vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0));
+		await sut.execute({
+			gymId: "gym-01",
+			userId: "user-01",
+		});
+
+		vi.setSystemTime(new Date(2022, 0, 21, 8, 0, 0));
+
 		const { checkIn } = await sut.execute({
 			gymId: "gym-01",
 			userId: "user-01",
--- src/services/check-in.ts
+++ src/services/check-in.ts
@@ -14,6 +14,15 @@ export class CheckInService {
 	constructor(private checkInsRepository: CheckInsRepository) {}
 
 	async execute({ userId, gymId }: CheckInRequest): Promise<CheckInResponse> {
+		const checkInOnSameDate = await this.checkInsRepository.findByUserIdOnDate(
+			userId,
+			new Date(),
+		);
+
+		if (checkInOnSameDate) {
+			throw new Error();
+		}
+
 		const checkIn = await this.checkInsRepository.create({
 			gym_id: gymId,
 			user_id: userId,
